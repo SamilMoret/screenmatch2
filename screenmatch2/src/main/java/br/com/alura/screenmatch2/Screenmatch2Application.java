22acdf5dd8711dae4389ceb8d46b3c43030cfdfa
Samil <samil.moret@gmail.com>
2024-06-11 22:38:07 -0300

    Initial commit

---
+++ screenmatch2/src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
@@ -0,0 +1,22 @@
+package br.com.alura.screenmatch2;
+
+import br.com.alura.screenmatch2.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatch2Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Screenmatch2Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal ();
+        principal.exibeMenu ();
+
+    }
+
+}
