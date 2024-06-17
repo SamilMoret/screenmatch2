86d865ad586e31eb8269b791fe5679dc617cfb99
Samil <samil.moret@gmail.com>
2024-06-17 08:54:44 -0300

    filtrando séries por vários requisitos

--- screenmatch2/src/main/java/br/com/alura/screenmatch2/principal/Principal.java
+++ screenmatch2/src/main/java/br/com/alura/screenmatch2/principal/Principal.java
@@ -1,9 +1,6 @@
 package br.com.alura.screenmatch2.principal;
 
-import br.com.alura.screenmatch2.model.DadosSerie;
-import br.com.alura.screenmatch2.model.DadosTemporada;
-import br.com.alura.screenmatch2.model.Episodio;
-import br.com.alura.screenmatch2.model.Serie;
+import br.com.alura.screenmatch2.model.*;
 import br.com.alura.screenmatch2.repository.SerieRepository;
 import br.com.alura.screenmatch2.service.ConsumoApi;
 import br.com.alura.screenmatch2.service.ConsultaGoogleTranslate;
@@ -43,6 +40,9 @@ public class Principal {
                     4 - Buscar série por título
                     5 - Buscar series por ator
                     6 - Top 5 Séries
+                    7 - Buscar séries por categoria
+                    8 - Filtrar séries
+                    
                                         
                     0 - Sair                                 
                     """;
@@ -70,6 +70,12 @@ public class Principal {
                 case 6:
                     buscarTop5Series();
                     break;
+                case 7:
+                    buscarSeriesPorCategoria();
+                    break;
+                case 8:
+                    filtrarSeriesPorTemporadaEAvaliacao();
+                    break;
                 case 0:
                     System.out.println ( "Saindo..." );
                     break;
@@ -78,6 +84,8 @@ public class Principal {
             }
         }
     }
+
+
 
     private void buscarSerieWeb () {
         DadosSerie dados = getDadosSerie ( );
@@ -167,6 +175,28 @@ public class Principal {
         List<Serie> serieTop = repositorio.findTop5ByOrderByAvaliacaoDesc();
         serieTop.forEach(s ->
                 System.out.println(s.getTitulo() + " avaliação: " + s.getAvaliacao()));
+    }
+
+    private void buscarSeriesPorCategoria() {
+        System.out.println("Deseja buscar séries de que categoria/gênero? ");
+        var nomeGenero = leitura.nextLine();
+        Categoria categoria = Categoria.fromPortugues ( nomeGenero );
+        List<Serie> seriesPorCategoria = repositorio.findByGenero(categoria);
+        System.out.println("Séries da categoria " + nomeGenero);
+        seriesPorCategoria.forEach(System.out::println);
+    }
+
+    private void filtrarSeriesPorTemporadaEAvaliacao(){
+        System.out.println("Filtrar séries até quantas temporadas? ");
+        var totalTemporadas = leitura.nextInt();
+        leitura.nextLine();
+        System.out.println("Com avaliação a partir de que valor? ");
+        var avaliacao = leitura.nextDouble();
+        leitura.nextLine();
+        List<Serie> filtroSeries = repositorio.findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(totalTemporadas, avaliacao);
+        System.out.println("*** Séries filtradas ***");
+        filtroSeries.forEach(s ->
+                System.out.println(s.getTitulo() + "  - avaliação: " + s.getAvaliacao()));
     }
 
 }
